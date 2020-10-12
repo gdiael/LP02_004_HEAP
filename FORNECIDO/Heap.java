ec5491f34cc7b0400b65a30e9638f408d7e841fb
Gdiael Souto Barros <gohan.brown@gmail.com>
2020-10-12 10:24:15 -0300

    Arquivos fornecidos pelo professor

---
+++ FORNECIDO/Heap.java
@@ -0,0 +1,29 @@
+
+public class Heap extends PriorityQueue {
+	
+	public static void main(String [] args){
+		File dir(".");
+		
+	}
+	
+	public Heap(int [] vetor){
+	}
+	
+	public void subir(int i){
+	}
+	
+	public boolean add(int value) {
+		elements[size] = value;
+		size += 1;
+		subir(size-1);
+	}
+	
+	public int remove() {
+	}
+	
+	public int get() {
+	}
+	
+	public void update(int value, int newValue) {
+	}
+}
